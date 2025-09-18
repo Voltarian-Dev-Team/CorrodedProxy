de9dc549598483e449c76928e8ffd507369bc8a0
VoltacceptYT <drifter@voltaccept.com>
2025-09-18 16:22:48 -0400

    Update 1.3.2

--- main/index.js
+++ main/index.js
@@ -1,8 +1,14 @@
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const server = http.createServer();
-const Corrosion = require('../');
+const certPath = path.join(__dirname, '../certs/cert.pem');
+const keyPath = path.join(__dirname, '../certs/key.pem');
+const credentials = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+};
+const server = https.createServer(credentials);
+const Corrosion = require('../lib/server');
 const proxy = new Corrosion({
     codec: 'xor',
     prefix: '/c/',
@@ -14,4 +20,4 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT || 7070); // we have to listen at a higher port because heroku
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT || 8080); // we have to listen at a higher port because heroku
