6769db1c461b50f4b027ac08b9da3fcf7db611ba
VoltacceptYT <drifter@voltaccept.com>
2025-09-18 17:59:21 -0400

    Implement code changes to enhance functionality and improve performance

--- main/index.js
+++ main/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
@@ -21,28 +20,4 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-=======
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const certPath = path.join(__dirname, '../certs/cert.pem');
-const keyPath = path.join(__dirname, '../certs/key.pem');
-const credentials = {
-    key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
-};
-const server = https.createServer(credentials);
-const Corrosion = require('../lib/server');
-const proxy = new Corrosion({
-    codec: 'xor',
-    prefix: '/c/',
-    forceHttps: true
-});
-
-proxy.bundleScripts();
-
-server.on('request', (request, response) => {
-    if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
-    response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
->>>>>>> 60224add77db3a8003ec0c12b8437c0e63548dfb
 }).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT || 8080); // we have to listen at a higher port because heroku
